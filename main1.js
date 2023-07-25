496c9c6e2f7b29af8121f81d208e32e9e228a98d
nachogilma <nachogilmaldonado@gmail.com>
2023-07-25 12:19:40 -0300

    primer push

---
+++ main1.js
@@ -0,0 +1,155 @@
+
+//let numero = parseInt(prompt("Ingrese el numero"));
+
+//for(let i = 1; i <= 10; i--){
+//console.log(7 * i);
+//}
+
+
+//for (let turno = 1; turno <= 7; turno++) {
+  //  let nombre = prompt ("ingrese el nombre");
+//}
+  //  alert('turno #${turno} Nombre: ${nombre}');
+
+
+//for(let i= 1; i <= 10; i++){
+    //if(i === 5){
+      //  continue;
+    //}
+    //console.log(i);
+//}
+
+/*let usuario = prompt("ingrese el usuario");
+
+while(usuario != "pepito"){
+    alert("usuario incorrecto");
+    usuario = prompt("ingrese el usuario")
+    
+}
+    alert("bienvenido")
+
+*/
+
+
+
+/*
+alert
+
+let moneda = prompt("ingrese el nombre de la moneda");
+
+switch(moneda) {
+    case "ars" :
+        console.log("moneda de argentina");
+        break;
+
+    case "clp":
+        console.log("moneda de chile")
+        break;
+
+    case "cop":
+        console.log("moneda de colombia")
+        break;
+
+        alert
+}
+
+*/
+
+/*let entrada = prompt("ingresar un nombre");
+
+while(entrada != "ESC"){
+    switch(entrada) {
+        case "ana":
+        alert("Hola ana")
+        break;
+
+        case "juan":
+        alert("Hola Juan");
+        break;
+
+        default:
+            alert("Quien eres?");
+            break;
+    }
+    entrada = prompt("ingresar un nombre");
+}
+
+/*for (let turno = 1; turno <= 7; turno++) {
+    let nombre = prompt("ingrese el nombre");
+} 
+*/
+
+
+    /*let numero = parseInt(prompt("ingrese el numero"));
+
+for(let i = 1; i <= 10; i++){
+    alert('${numero} X ${i} = ${numero i}');
+}
+
+let edad = parseInt(prompt("ingrese su edad"));
+
+alert(edad + 5)
+
+
+for (let turno = 1; turno <= 7; turno++){
+    let nombre = prompt("ingrese el nombre");
+}   
+    alert('turno #${turno} nombre: ${nombre}')
+
+    
+
+    let texto = prompt("ingrese el texto"); //andres
+    let resultado = texto; //andres
+    let nuevotexto = "";
+
+    while (texto != "ESC"){
+        let nuevotexto = prompt("Ingrese el nuevo texto"); //felipe
+        resultado = resultado + " " + nuevotexto;
+        alert(resultado);
+    }
+
+    */
+
+    /*let numero = parseInt(prompt("ingrese el numero"));
+    let cantidadDivisores = 0
+
+    for(let i = 1; i <= numero; i++){
+        if (numero % i === 0) {
+            cantidadDivisores++;
+        }
+    }
+
+    if (cantidadDivisores === 2){
+        alert("El numero no es primo");
+    }
+    else{
+        alert(" El numero es primo");
+    }
+
+    */
+
+    /*
+    estructura de una funcion
+    
+    function nombreFuncion(){
+        codigo a ejecutar por la funcion
+    }
+    */
+
+    //function saludar () {
+       // console.log("Hola");
+   // }
+
+    /*    function saludar() {
+        let apellido = prompt("ingrese el apellido");
+        alert('su appelido es $(apellido)');
+    }
+
+    saludar();
+    */ 
+
+    
+    //function sumar(num1 , num2) {
+      //  console.log(num1 + num2);
+    //}
+   // sumar(9, 3);
