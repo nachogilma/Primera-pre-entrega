f60caa138abf1a83beb8f6793073f6d78bba72a0
nachogilma <nachogilmaldonado@gmail.com>
2023-07-30 21:39:19 -0300

    casi

--- main.js
+++ main.js
@@ -23,25 +23,25 @@ let edad = prompt("ingrese su fecha de cumpleaños para conseguir beneficios, si
     alert("Gracias por confiar en nosotros a continuacion puede elegir entre todas estas");
 }
 
-let respuesta = prompt("Si desea una pizza individual escriba 1 , si desea seleccionar un combo escriba 2 (todas nuestras pizzas estan elaboradas con hornos de piedra)")
+let respuesta = prompt(" Si desea una pizza individual escriba 1 , si desea seleccionar un combo escriba 2 (todas nuestras pizzas estan elaboradas con hornos de piedra)")
 
-if(respuesta === 1){
-    
+if(respuesta === '1' ){
+    let individual = prompt(" 1) Pizza muzzarela 3500$   2) Pizza cuatro quesos 3700$   3) Pizza fugazzeta 3700$  4) Pizza cuatro estaciones 3800$  5) Pizza con champiñones 3700$    6) Pizza Napolitana 3650$")
     //codigo a ejecutar cuando la condicion1 sea verdadera
 
-}else if(respuesta === 2){
+}else if(respuesta === '2' ){
+    let combo = prompt(" 1) dos pizzas muzzarela 6500$   2) Una pizza muzzarela y una napolitana 5500$    3) Pizza cuatro estaciones y cuatro quesos 7000$    4) Pizza fugazzeta y una Pizza con champiñones 6800$")
 
     //codigo a ejecutar cuando la condicion2 sea verdadera
 
 }else{
     alert("Porfavor escriba correctamente 1 o 2");
-
-    let respuesta = prompt("Si desea una pizza individual escriba 1 , si desea seleccionar un combo escriba 2 (todas nuestras pizzas estan elaboradas con hornos de piedra)")
-
-    if(respuesta === 1){
-    
-    //codigo a ejecutar cuando la condicion1 sea verdadera
-
-    }else if(respuesta === 2){
     //codigio a ejecutar cuando ninguna de las condiciones sean verdaderas
-} 
+    let respuesta = prompt("Si desea una pizza individual escriba 3 , si desea seleccionar un combo escriba 4 (todas nuestras pizzas estan elaboradas con hornos de piedra)")
+
+}if (respuesta === '3' ) {
+    let individual = prompt(" 1) Pizza muzzarela 3500$   2) Pizza cuatro quesos 3700$  3) Pizza fugazzeta 3700$  4) Pizza cuatro estaciones 3800$  5) Pizza con champiñones 3700$    6) Pizza Napolitana 3650$")
+}else if (respuesta === '4' ) {
+    let combo = prompt(" 1) dos pizzas muzzarela 6500$   2) Una pizza muzzarela y una napolitana 5500$    3) Pizza cuatro estaciones y cuatro quesos 7000$    4) Pizza fugazzeta y una Pizza con champiñones 6800$") 
+    
+}
